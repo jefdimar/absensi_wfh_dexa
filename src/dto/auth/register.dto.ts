e338f23bbb1a2f34c78f28444198757969d90dd0
jefdimar <jefdimar@gmail.com>
2025-06-20 16:37:41 +0700

    create DTOs for auth

---
+++ src/dto/auth/register.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsOptional,
+  Matches,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @MaxLength(100)
+  name: string;
+
+  @IsEmail()
+  @MaxLength(100)
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{6,}$/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  position?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  photoUrl?: string;
+}
