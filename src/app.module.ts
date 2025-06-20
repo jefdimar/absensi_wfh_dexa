73abb25572d70dcee9b3b2ba583ae11507cd1e04
jefdimar <jefdimar@gmail.com>
2025-06-20 15:53:02 +0700

    appmodule to wire everything together

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfileChangeLogController } from './controllers/profile-change-log.controller';
+import { ProfileChangeLogService } from './services/profile-change-log.service';
+import { ProfileChangeLog } from './entities/profile-change-log.entity';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forFeature([ProfileChangeLog]),
+  ],
+  controllers: [AppController, ProfileChangeLogController],
+  providers: [AppService, ProfileChangeLogService],
 })
 export class AppModule {}
