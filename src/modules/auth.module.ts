8f65557784d7dc6ca294339156cfa25559ad9c8b
jefdimar <jefdimar@gmail.com>
2025-06-20 16:41:46 +0700

    create auth modules

---
+++ src/modules/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from '../controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
+import { Employee } from '../entities/employee.entity';
+import { ProfileChangeLog } from '../entities/profile-change-log.entity';
+import { ProfileChangeLogService } from '../services/profile-change-log.service';
+import { JwtStrategy } from '../strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Employee, ProfileChangeLog]),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET', 'your-secret-key'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN', '24h') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, ProfileChangeLogService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
