c65d76485f98ffeef439c1312a3c15a1b43201f3
jefdimar <jefdimar@gmail.com>
2025-06-20 16:36:39 +0700

    adding dependency and employee entity

---
+++ src/entities/employee.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('employees')
+@Index('idx_employees_email', ['email'])
+export class Employee {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', { length: 100 })
+  name: string;
+
+  @Column('varchar', { length: 100, unique: true })
+  email: string;
+
+  @Column('text', { name: 'password_hash' })
+  @Exclude()
+  passwordHash: string;
+
+  @Column('text', { nullable: true, name: 'photo_url' })
+  photoUrl: string;
+
+  @Column('varchar', { length: 100, nullable: true })
+  position: string;
+
+  @Column('varchar', { length: 20, nullable: true, name: 'phone_number' })
+  phoneNumber: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
