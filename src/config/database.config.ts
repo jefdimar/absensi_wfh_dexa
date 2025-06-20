81c3af85ee212b8b6a23663825cbc0b0ec32c861
jefdimar <jefdimar@gmail.com>
2025-06-20 15:48:50 +0700

    adding config database & main.ts setup

---
+++ src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'absensi_wfh_dexa',
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV === 'development',
+};
